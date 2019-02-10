ec028acd36ba92ec7d1b445d51ed9d4e822b9c96
Germano Barcelos <gegenbarcelos@gmail.com>
2019-02-10 15:38:51 +0000

    Updating docker-compose.yml

---
+++ app/cartola-coltec-service/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main () {
+	router := mux.NewRouter()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello from Golang")
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
