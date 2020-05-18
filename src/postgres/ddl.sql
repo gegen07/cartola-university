63099d6e29781a6ed89111934570217e18978b1d
Germano Barcelos <gegenbarcelos@gmail.com>
2020-05-17 22:52:25 -0300

    scripting database

--- src/postgres/ddl.sql
+++ src/postgres/ddl.sql
@@ -1,117 +1,38 @@
-DROP TABLE IF EXISTS formation CASCADE;
+DROP TABLE IF EXISTS formation cascade;
 CREATE TABLE formation (
-    id          SERIAL PRIMARY KEY,
-    goalkeeper  SMALLINT NOT NULL,
-    defenders   SMALLINT NOT NULL,
-    attackers   SMALLINT NOT NULL
+	id          SERIAL PRIMARY KEY,
+	goalkeeper  SMALLINT NOT NULL,
+	defenders   SMALLINT NOT NULL,
+	attackers   SMALLINT NOT null,
+	created_at TIMESTAMPTZ(6) NOT null,
+	updated_at timestamptz(6) NOT null
 );
 
-DROP TABLE IF EXISTS round CASCADE;
-CREATE TABLE round (
-    id                  SERIAL PRIMARY KEY,
-    round_begin_date    TIMESTAMP NOT NULL,
-    round_finish_date   TIMESTAMP NOT NULL
+
+DROP TABLE IF EXISTS scouts cascade;
+CREATE TABLE scouts (
+    id SERIAL PRIMARY KEY,
+    scout varchar(255) NOT null,
+    description text NOT null,
+    created_at TIMESTAMPTZ(6) NOT null,
+    updated_at timestamptz(6) NOT null
 );
 
-DROP TABLE IF EXISTS scout CASCADE;
-CREATE TABLE scout (
-    id              SERIAL PRIMARY KEY,
-    description     VARCHAR(80) NOT NULL,
-    points          SMALLINT NOT NULL
+DROP TABLE IF EXISTS positions cascade;
+CREATE TABLE positions (
+    id SERIAL PRIMARY KEY,
+    description varchar(255)  NOT NULL,
+    created_at TIMESTAMPTZ(6) NOT null,
+    updated_at timestamptz(6) NOT null
 );
 
-DROP TABLE IF EXISTS team CASCADE;
-CREATE TABLE team (
-    id          SERIAL PRIMARY KEY,
-    name        VARCHAR(255) NOT NULL,
-    nickname    VARCHAR(255) NOT NULL,
-    image_url   VARCHAR(255) NOT NULL
+drop table if exists scout_positions cascade; 
+create table scout_positions (
+	id serial primary key,
+	position_id integer references positions(id),
+	scout_id integer references scouts(id),
+	created_at TIMESTAMPTZ(6) NOT null,
+    updated_at timestamptz(6) NOT null
 );
 
-DROP TABLE IF EXISTS team_stats CASCADE;
-CREATE TABLE team_stats (
-    id              SERIAL PRIMARY KEY,
-    id_team         INT REFERENCES team(id) ON DELETE CASCADE,
-    victory         SMALLINT NOT NULL,
-    lose            SMALLINT NOT NULL,
-    draw            SMALLINT NOT NULL,
-    goal_against    SMALLINT NOT NULL,
-    goal_difference SMALLINT NOT NULL,
-    goal_pro        SMALLINT NOT NULL
-);
 
-DROP TABLE IF EXISTS player CASCADE;
-CREATE TABLE player (
-    id          SERIAL PRIMARY KEY,
-    id_team     INT REFERENCES team(id) ON DELETE CASCADE,
-    name        VARCHAR(255) NOT NULL,
-    nickname    VARCHAR(255) NOT NULL,
-    photo       VARCHAR(255) NOT NULL,
-    price       DECIMAL NOT NULL,
-    score       DECIMAL NOT NULL,
-    median      DECIMAL NOT NULL,
-    num_matches SMALLINT NOT NULL,
-    position    VARCHAR(255) NOT NULL,
-    status      VARCHAR(255) NOT NULL
-
-);
-
-DROP TABLE IF EXISTS player_stats CASCADE;
-CREATE TABLE player_stats (
-    id              SERIAL PRIMARY KEY,
-    round_id        INT REFERENCES round(id) ON DELETE CASCADE,
-    player_id       INT REFERENCES player(id) ON DELETE CASCADE,
-    scout_id        INT REFERENCES scout(id) ON DELETE CASCADE,
-    amount_scout    SMALLINT  NOT NULL
-);
-
-DROP TABLE IF EXISTS match CASCADE;
-CREATE TABLE match (
-    id                      SERIAL PRIMARY KEY,
-    id_home_team            INT REFERENCES team(id) ON DELETE CASCADE,
-    id_visitor_team         INT REFERENCES team(id) ON DELETE CASCADE,
-    scoreboard_home_team    SMALLINT NOT NULL,
-    scoreboard_visitor_team SMALLINT NOT NULL,
-    match_date              TIMESTAMP NOT NULL
-);
-
-DROP TABLE IF EXISTS round_matches CASCADE;
-CREATE TABLE round_matches (
-    id          SERIAL PRIMARY KEY,
-    id_round    INT REFERENCES round(id) ON DELETE CASCADE,
-    id_match    INT REFERENCES match(id) ON DELETE CASCADE
-);
-
-DROP TABLE IF EXISTS cup CASCADE;
-CREATE TABLE cup (
-    id                 SERIAL PRIMARY KEY,
-    id_winner_team     INT REFERENCES team(id) ON DELETE CASCADE,
-    id_second_team     INT REFERENCES team(id) ON DELETE CASCADE,
-    id_third_team      INT REFERENCES team(id) ON DELETE CASCADE,
-    id_current_round   INT REFERENCES round(id) ON DELETE CASCADE,
-    year               INT NOT NULL,
-    begin_date         TIMESTAMP NOT NULL,
-    finish_date        TIMESTAMP NOT NULL,
-    name_cup           VARCHAR(255) NOT NULL
-);
-
-DROP TABLE IF EXISTS cup_rounds CASCADE;
-CREATE TABLE cup_rounds (
-    id          SERIAL PRIMARY KEY;
-    id_cup      INT REFERENCES cup(id) ON DELETE CASCADE;
-    id_round    INT REFERENCES round(id) ON DELETE CASCADE;
-);
-
-DROP TABLE IF EXISTS shop CASCADE;
-CREATE TABLE shop (
-    id          SERIAL PRIMARY KEY,
-    id_cup      INT REFERENCES cup(id) ON DELETE CASCADE,
-    status      VARCHAR(255) NOT NULL
-);
-
-DROP TABLE IF EXISTS players_shop CASCADE;
-CREATE TABLE players_shop (
-    id          SERIAL PRIMARY KEY,
-    id_player   INT REFERENCES player(id) ON DELETE CASCADE,
-    id_shop     INT REFERENCES shop(id) ON DELETE CASCADE
-);
