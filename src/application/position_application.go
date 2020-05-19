98d8c8c6e38ccd247d43da55f658bbb8d58a9106
Germano Barcelos <gegenbarcelos@gmail.com>
2020-05-19 16:06:15 -0300

    refactoring position application

--- src/application/position_application.go
+++ src/application/position_application.go
@@ -1,45 +1,112 @@
 package application
 
 import (
+	"context"
 	"github.com/gegen07/cartola-university/domain/entity/scout"
 	"github.com/gegen07/cartola-university/domain/repository"
+	"golang.org/x/sync/errgroup"
+	"time"
 )
 
 type PositionApplicationInterface interface {
-	GetAll(args...interface{}) ([]scout.Position, error)
-	GetById(id uint64) (*scout.Position, error)
-	Insert(position *scout.Position) (*scout.Position, error)
-	Update(position *scout.Position) (*scout.Position, error)
-	Delete(id uint64) error
-	AppendScoutAssociation(position *scout.Position, scout *scout.Scout) error
+	GetAll(ctx context.Context, page int) ([]scout.Position, error)
+	GetById(ctx context.Context, id uint64) (*scout.Position, error)
+	Insert(ctx context.Context, position *scout.Position) (*scout.Position, error)
+	Update(ctx context.Context, position *scout.Position) (*scout.Position, error)
+	Delete(ctx context.Context, id uint64) error
 }
 
 type PositionApplication struct {
-	repo repository.PositionRepository
-}
-
-func (p *PositionApplication) AppendScoutAssociation(position *scout.Position, scout *scout.Scout) error {
-	return p.repo.AppendScoutAssociation(position, scout)
-}
-
-func (p *PositionApplication) GetAll(args ...interface{}) ([]scout.Position, error) {
-	return p.repo.GetAll(args)
-}
-
-func (p *PositionApplication) GetById(id uint64) (*scout.Position, error) {
-	return p.repo.GetById(id)
-}
-
-func (p *PositionApplication) Insert(position *scout.Position) (*scout.Position, error) {
-	return p.repo.Insert(position)
-}
-
-func (p *PositionApplication) Update(position *scout.Position) (*scout.Position, error) {
-	return p.repo.Update(position)
-}
-
-func (p *PositionApplication) Delete(id uint64) error {
-	return p.repo.Delete(id)
+	repo                    repository.PositionRepository
+	scoutPositionRepository repository.ScoutPositionRepository
+	contextTimeout time.Duration
 }
 
 var _ PositionApplicationInterface = &PositionApplication{}
+
+func (p *PositionApplication) fillPositionWithScout(c context.Context, data []scout.Position) ([]scout.Position, error) {
+	g, ctx := errgroup.WithContext(c)
+
+	for _, position := range data  {
+		g.Go(func() error {
+			res, err := p.scoutPositionRepository.GetScoutsByPositionID(ctx, position.ID)
+
+			if err != nil {
+				return err
+			}
+
+			position.Scouts = res
+
+			return nil
+		})
+	}
+
+	go func() {
+		err := g.Wait()
+
+		if err != nil {
+			return
+		}
+	}()
+
+	if err := g.Wait(); err!= nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+func (p *PositionApplication) GetAll(ctx context.Context, page int) ([]scout.Position, error) {
+	ctx, cancel := context.WithTimeout(ctx, p.contextTimeout)
+	defer cancel()
+
+	positions, err := p.repo.GetAll(ctx, page)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return p.fillPositionWithScout(ctx, positions)
+}
+
+func (p *PositionApplication) GetById(ctx context.Context,id uint64) (*scout.Position, error) {
+	ctx, cancel := context.WithTimeout(ctx, p.contextTimeout)
+	defer cancel()
+
+	position, err := p.repo.GetById(ctx, id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := p.scoutPositionRepository.GetScoutsByPositionID(ctx, position.ID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	position.Scouts = res
+
+	return position, nil
+}
+
+func (p *PositionApplication) Insert(ctx context.Context, position *scout.Position) (*scout.Position, error) {
+	ctx, cancel := context.WithTimeout(ctx, p.contextTimeout)
+	defer cancel()
+
+	return p.repo.Insert(ctx, position)
+}
+
+func (p *PositionApplication) Update(ctx context.Context, position *scout.Position) (*scout.Position, error) {
+	ctx, cancel := context.WithTimeout(ctx, p.contextTimeout)
+	defer cancel()
+
+	return p.repo.Update(ctx, position)
+}
+
+func (p *PositionApplication) Delete(ctx context.Context, id uint64) error {
+	ctx, cancel := context.WithTimeout(ctx, p.contextTimeout)
+	defer cancel()
+
+	return p.repo.Delete(ctx, id)
+}
--- src/go.mod
+++ src/go.mod
@@ -5,7 +5,9 @@ go 1.13
 require (
 	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.4
-	github.com/jinzhu/gorm v1.9.12
+	github.com/jinzhu/gorm v1.9.12 // indirect
 	github.com/joho/godotenv v1.3.0
-	github.com/shopspring/decimal v0.0.0-20200227202807-02e2044944cc
+	github.com/lib/pq v1.1.1
+	github.com/shopspring/decimal v0.0.0-20200227202807-02e2044944cc // indirect
+	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a
 )
